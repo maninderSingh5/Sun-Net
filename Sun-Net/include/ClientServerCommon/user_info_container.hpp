3ca3e64aa5fa3289fcedf4f62d05e3b2a45fcb5b
maninderSingh5 <147925569+maninderSingh5@users.noreply.github.com>
2024-03-07 14:36:24 +0530

    Update user_info_container.hpp

--- Sun-Net/include/ClientServerCommon/user_info_container.hpp
+++ Sun-Net/include/ClientServerCommon/user_info_container.hpp
@@ -1,1 +1,36 @@
+#ifndef USER_INFO_CONTAINER_HPP
+#define USER_INFO_CONTAINER_HPP
 
+#include "sun_net.hpp"
+
+namespace sun
+{
+	namespace net
+	{
+		template <typename T>
+		struct user_info
+		{
+			std::string m_user_id;
+			std::string m_userName;
+			std::vector<std::string> m_rooms;
+			std::vector<std::string> m_roomRequestsIn;
+			std::vector<std::string> m_roomsRequestOut;
+			
+			user_info(std::string id, std::string name)
+			: m_user_id(id), m_userName(name)
+			{}
+			
+			void Serialize(message<T>& mesg)
+			{
+				//implementation pending
+			}
+			
+			void DeSerilaize(message<T>& mesg)
+			{
+				//implementation pending
+			}
+		};
+	}
+}
+
+#endif 
