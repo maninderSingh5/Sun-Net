4fa8515ce9982498440d20e57247662c18c449b4
Maninder Singh <maninder21303103@gmail.com>
2024-03-26 13:06:34 -0700

    jkl

---
+++ SunServer/include/Attributes.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef ATTRIBUTES_HPP
+#define ATTRIBUTES_HPP
+
+#include "sun_net.hpp"
+#include "connection.hpp"
+
+namespace sun
+{
+	struct UserID : std::string
+	{
+
+	};
+
+
+}
+#endif
