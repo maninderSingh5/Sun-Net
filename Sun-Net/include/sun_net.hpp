1bf0c450036e273df1d918dcbd6adf0b971001a3
maninderSingh5 <147925569+maninderSingh5@users.noreply.github.com>
2024-03-28 01:20:41 +0530

    Update sun_net.hpp

--- Sun-Net/include/sun_net.hpp
+++ Sun-Net/include/sun_net.hpp
@@ -1,9 +1,14 @@
+#ifndef SUN_NET_HPP
+#define SUN_NET_HPP
+
 #include "common.hpp"
 #include "message.hpp"
 #include "netClient.hpp"
 #include "netServer.hpp"
-#include "ClientServerCommon/NetAttributes.hpp"
 #include "ClientServerCommon/flags.hpp"
 #include "ClientServerCommon/Database.hpp"
 #include "ClientServerCommon/user_info_container.hpp"
 #include "ClientServerCommon/file_manager.hpp"
+#include "ClientServerCommon/NetAttributes.hpp"
+
+#endif
